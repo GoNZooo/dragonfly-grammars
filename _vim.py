bb3eae5c5a6a6c258b8dffb182d18f606f668f9c
Rickard Andersson <gonz@severnatazvezda.com>
2020-07-17 19:01:47 +0300

    modify vim contexts

--- _vim.py
+++ _vim.py
@@ -23,10 +23,16 @@ putty_context = AppContext(executable="putty")
 code_context = AppContext(executable="code")
 firefox_context = AppContext(executable="firefox")
 discord_context = AppContext(executable="discord")
-franz_context = AppContext(executable="Franz")
 powerbash_context = AppContext(executable="powershell")
-vim_context = (putty_context | code_context | firefox_context |
-               discord_context | franz_context | powerbash_context)
+cmder_context = AppContext(executable="Cmder")
+graphical_neovim_context = AppContext(executable="nvim-qt")
+terminal_preview_context = AppContext(executable="WindowsTerminal")
+ripcord_context = AppContext(executable="Ripcord")
+skype_context = AppContext(title="Skype")
+edge_context = AppContext(executable="msedge")
+vim_context = (putty_context | code_context | firefox_context | terminal_preview_context |
+               discord_context | powerbash_context | cmder_context | graphical_neovim_context |
+               skype_context | ripcord_context | edge_context)
 
 # the bootstrapper loads the grammar
 VimBootstrap = Grammar("vim bootstrap", context=vim_context)
