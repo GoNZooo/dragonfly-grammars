af9617d8b3acfa011bd62b3599eebe871fa1bd78
Rickard Andersson <gonz@severnatazvezda.com>
2020-07-17 19:03:55 +0300

    add zig

---
+++ _zig.py
@@ -0,0 +1,81 @@
+from dragonfly import (Grammar, CompoundRule, Text, MappingRule)
+from macro_utilities import (replace_percentage)
+
+
+def insert_function():
+    return Text("fn %%(_) _ {}") + replace_percentage(25)
+
+
+class ZigEnabler(CompoundRule):
+    spec = "all your base are belong to us"
+
+    def _process_recognition(self, node, extras):
+        zigBootstrap.disable()
+        zigGrammar.enable()
+        print "Zig grammar enabled!"
+
+
+class ZigDisabler(CompoundRule):
+    spec = "take off every zig"
+
+    def _process_recognition(self, node, extras):
+        zigGrammar.disable()
+        zigBootstrap.enable()
+        print "Zig grammar disabled"
+
+
+class ZigUtilities(MappingRule):
+    mapping = {
+        "if": Text("if (%%) {} else {}") + replace_percentage(15),
+        "for loop": Text("for (%%) |_| {}") + replace_percentage(15),
+        "while loop": Text("while (%%) (_) {}") + replace_percentage(15),
+        "switch": Text("switch (%%) {}") + replace_percentage(15),
+
+        "const": Text("const %% = ;") + replace_percentage(15),
+        "variable": Text("var %% = ;") + replace_percentage(15),
+        "exported function": Text("pub ") + insert_function(),
+        "function": insert_function(),
+        "a sink": Text("async "),
+        "await": Text("await %%;") + replace_percentage(15),
+        "try": Text("try %%;") + replace_percentage(15),
+        "catch": Text("catch "),
+        "defer": Text("defer %%;") + replace_percentage(15),
+        "import": Text("const %% = @import(_);") + replace_percentage(15),
+        "compile time": Text("comptime "),
+
+        "define struct": Text("const %% = struct {};") + replace_percentage(15),
+        "define union": Text("const %% = union(_) {};") + replace_percentage(15),
+        "define enumeration": Text("const %% = enum {};") + replace_percentage(15),
+        "struct": Text("struct {}"),
+        "union": Text("union(_) {}"),
+        "enumeration": Text("enum {}"),
+
+        "check equal": Text(" == "),
+        "check not equal": Text(" != "),
+        "equals": Text(" = "),
+        "arrow": Text(" => "),
+        "pointer": Text("ptr"),
+    }
+
+    extras = []
+
+
+# The main Zig grammar rules are activated here
+zigBootstrap = Grammar("zig bootstrap")
+zigBootstrap.add_rule(ZigEnabler())
+zigBootstrap.load()
+
+zigGrammar = Grammar("zig grammar")
+zigGrammar.add_rule(ZigUtilities())
+zigGrammar.add_rule(ZigDisabler())
+zigGrammar.load()
+zigGrammar.disable()
+
+# Unload function which will be called by natlink at unload time.
+
+
+def unload():
+    global zigGrammar
+    if zigGrammar:
+        zigGrammar.unload()
+    zigGrammar = None
