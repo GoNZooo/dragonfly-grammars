0559c2ae03e872ece5e45f433c75276a040cbf22
Rickard Andersson <gonz@severnatazvezda.com>
2020-08-31 00:10:36 +0300

    pass down sleep value to `Key` properly

--- macro_utilities.py
+++ macro_utilities.py
@@ -1,9 +1,9 @@
-from dragonfly import (Key, Text, Pause)
+from dragonfly import (Key, Text)
 
 
 def sleep_exit(sleep):
-    return Key("escape") + Pause(str(sleep))
+    return Key("escape/%d" % sleep)
 
 
 def replace_percentage(sleep):
-    return sleep_exit(25) + Text("F%%s")
+    return sleep_exit(sleep) + Text("F%%s")
