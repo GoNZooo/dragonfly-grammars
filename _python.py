a9e6bdcfe1620bce116c9f778633af603293c6e2
Rickard Andersson <gonz@severnatazvezda.com>
2020-09-02 18:32:42 +0300

    [python] add more optional identifiers
    If one knows what an identifier should be from the beginning, one can just say it and it will be formatted correctly for the given type of thing it is.

--- _python.py
+++ _python.py
@@ -1,6 +1,6 @@
 from dragonfly import (Grammar, CompoundRule, Text, Key, MappingRule, Dictation, Function)
 from macro_utilities import (replace_in_text)
-from vim.rules.letter import (snake_case)
+from vim.rules.letter import (snake_case, proper)
 
 
 class PythonEnabler(CompoundRule):
@@ -24,11 +24,37 @@ class PythonDisabler(CompoundRule):
 def output_function(function_name):
     if function_name == "":
         command = replace_in_text("def $(_):")
-        command.execute()
     else:
         function_name = snake_case(str(function_name))
         command = replace_in_text("def %s($):" % function_name)
-        command.execute()
+    command.execute()
+
+
+def output_class(class_name):
+    if class_name == "":
+        command = replace_in_text("class $:")
+    else:
+        class_name = proper(str(class_name))
+        command = Text("class %s:" % class_name) + Key("enter")
+    command.execute()
+
+
+def output_from_import(import_name):
+    if import_name == "":
+        command = replace_in_text("from $ import (_)")
+    else:
+        import_name = snake_case(str(import_name))
+        command = replace_in_text("from %s import ($)" % import_name)
+    command.execute()
+
+
+def output_qualified_import(import_name):
+    if import_name == "":
+        command = replace_in_text("import $ as _")
+    else:
+        import_name = snake_case(str(import_name))
+        command = replace_in_text("import %s as $" % import_name)
+    command.execute()
 
 
 class PythonUtilities(MappingRule):
@@ -39,11 +65,11 @@ class PythonUtilities(MappingRule):
         "for loop": replace_in_text("for $ in _:"),
         "while loop": replace_in_text("while $:"),
         "pass": Text("pass"),
-        "class": replace_in_text("class $:"),
+        "class [<class_name>]": Function(output_class),
         "anonymous function": replace_in_text("lambda $: "),
         "function [<function_name>]": Function(output_function),
-        "from import": replace_in_text("from $ import (_)"),
-        "qualified import": replace_in_text("import $ as _"),
+        "from [<import_name>] import": Function(output_from_import),
+        "qualified import [<import_name>]": Function(output_qualified_import),
         "import dragonfly": Text("import dragonfly as dragonfly"),
         "check equal": Text(" == "),
         "check not equal": Text(" != "),
@@ -51,7 +77,9 @@ class PythonUtilities(MappingRule):
     }
 
     extras = [
-        Dictation("function_name", default="")
+        Dictation("function_name", default=""),
+        Dictation("class_name", default=""),
+        Dictation("import_name", default="")
     ]
 
 
