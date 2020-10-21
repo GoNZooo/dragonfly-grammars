ac436bec46b90dcfe7e6493757d1dfeb2a46923e
Rickard Andersson <gonz@severnatazvezda.com>
2020-10-21 13:21:29 +0300

    add comparisons to go layer

--- _go.py
+++ _go.py
@@ -1,4 +1,4 @@
-from dragonfly import (CompoundRule, MappingRule, Grammar, Function, Choice, Dictation, Text)
+from dragonfly import (CompoundRule, MappingRule, Grammar, Function, Choice, Dictation, Text, Key)
 from macro_utilities import (execute_with_dictation, replace_in_text)
 from vim.rules.letter import (camel_case, proper)
 
@@ -50,6 +50,45 @@ def output_type(name,  construct, visibility_attribute=None):
     )
 
 
+comparison_choice_map = {
+    "equal": "==",
+    "not equal": "!=",
+    "less or equal": "<=",
+    "greater or equal": ">=",
+    "less": "<",
+    "greater": ">",
+    "none": "== nil",
+    "not none": "!= nil",
+}
+
+
+def comparison_choice(name="comparison"):
+    return Choice(name, comparison_choice_map)
+
+
+def output_if(name, statement_type):
+    execute_with_dictation(
+        name,
+        on_dictation=lambda v: Text("%s %s {" % (statement_type, format_variable_name(name))),
+        on_other=lambda v: replace_in_text("%s $ {" % statement_type),
+    )
+
+
+def output_if_comparison(name, construct, comparison=None):
+    if comparison is not None:
+        execute_with_dictation(
+            name,
+            on_dictation=lambda v: replace_in_text(
+                "%s %s %s $ {" % (construct, format_variable_name(v), comparison)
+            ),
+            on_other=lambda v: replace_in_text("%s $ %s _ {" % (construct, comparison))
+        )
+
+
+def format_variable_name(name):
+    return camel_case(str(name).replace("-", ""))
+
+
 def format_name(name, visibility_attribute):
     if visibility_attribute is None or visibility_attribute == "private":
         return camel_case(str(name).replace("-", ""))
@@ -59,6 +98,14 @@ def format_name(name, visibility_attribute):
 
 class GoUtilities(MappingRule):
     mapping = {
+        "if [<name>] is <comparison>": Function(output_if_comparison, construct="if"),
+        "if [<name>]": Function(output_if, statement_type="if"),
+
+        "else if [<name>]": Function(output_if, statement_type="else if"),
+        "else if [<name>] is <comparison>": Function(output_if_comparison, construct="else if"),
+
+        "else": Text("else {") + Key("enter"),
+
         "[<visibility_attribute>] function [<name>]": Function(output_function),
         "[<visibility_attribute>] struct [<name>]": Function(output_type, construct="struct"),
         "[<visibility_attribute>] interface [<name>]": Function(output_type, construct="interface"),
@@ -67,6 +114,7 @@ class GoUtilities(MappingRule):
     extras = [
         Dictation("name", default=""),
         visibility_attribute_choice("visibility_attribute"),
+        comparison_choice("comparison"),
     ]
 
 
